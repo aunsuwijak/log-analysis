139107a0e93d8c8738d2e81d036b14fc74cdf23f
Suwijak Chaipipat <ppnk2537@gmail.com>
2016-04-06 02:55:27 +0700

    Implement user_counting_pending_messages and add to app.rb

--- app.rb
+++ app.rb
@@ -1,7 +1,8 @@
 require_relative 'lib/file_reader.rb'
+require_relative 'lib/user_counting_pending_messages'
 
 reader = FileReader.new
 data = reader.load('sample.log')
 
-p data[0]
-p data[0].response
+user_counting_pending_messages = UserCountingPendingMessages.new
+user_counting_pending_messages.analysis(data)
