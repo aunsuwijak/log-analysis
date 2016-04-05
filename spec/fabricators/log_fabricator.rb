6d241c5d83797d6e122d1a4099df1bebff6f8012
Suwijak Chaipipat <ppnk2537@gmail.com>
2016-04-06 02:55:04 +0700

    Create user_counting_pending_messages_spec

--- spec/fabricators/log_fabricator.rb
+++ spec/fabricators/log_fabricator.rb
@@ -11,3 +11,17 @@ Fabricator(:log, class_name: Log) do
   status '200'
   bytes '78'
 end
+
+Fabricator(:user_counting_pending_messages_log, class_name: Log) do
+  created_at { Date.today }
+  at 'info'
+  method 'GET'
+  path '/api/users/12/count_pending_messages'
+  host 'services.pocketplaylab.com'
+  fwd '\"94.66.255.106\"'
+  dyno 'web.12'
+  connect '12ms'
+  service '21ms'
+  status '200'
+  bytes '78'
+end
