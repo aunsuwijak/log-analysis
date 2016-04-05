2dbc179ca440fc0b0788cda7ccee4a56043271a0
Suwijak Chaipipat <ppnk2537@gmail.com>
2016-04-06 03:09:36 +0700

    Implement user_get_messages and add to app.rb

--- app.rb
+++ app.rb
@@ -1,8 +1,12 @@
 require_relative 'lib/file_reader.rb'
 require_relative 'lib/user_counting_pending_messages'
+require_relative 'lib/user_get_messages'
 
 reader = FileReader.new
 data = reader.load('sample.log')
 
 user_counting_pending_messages = UserCountingPendingMessages.new
 user_counting_pending_messages.analysis(data)
+
+user_get_message = UserGetMessage.new
+user_get_message.analysis(data)
