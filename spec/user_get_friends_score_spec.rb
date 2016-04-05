fd838d46e1239e60a180e5ffa90cfc2de543fb9e
Suwijak Chaipipat <ppnk2537@gmail.com>
2016-04-06 03:24:10 +0700

    Create user_get_friends_score spec

---
+++ spec/user_get_friends_score_spec.rb
@@ -0,0 +1,31 @@
+require_relative 'spec_helper.rb'
+
+describe UserGetFriendsScore do
+
+  describe '#analysis' do
+    let(:log_1) { Fabricate(:user_get_friends_score_log, connect: '20', service: '20', dyno: 'web.2') }
+    let(:log_2) { Fabricate(:user_get_friends_score_log, connect: '20', service: '10', dyno: 'web.2') }
+    let(:log_3) { Fabricate(:user_get_friends_score_log, connect: '20', service: '10', dyno: 'web.2') }
+    let(:log_4) { Fabricate(:user_get_friends_score_log, connect: '10', service: '20', dyno: 'web.1') }
+    let(:log_5) { Fabricate(:log, connect: '20', service: '20', dyno: 'web.3') }
+
+    subject { UserGetFriendsScore.new }
+
+    context 'output' do
+      it do
+        expect {
+          subject.analysis([log_1, log_2, log_3, log_4])
+        }.to output(
+          [
+            "\"GET /api/users/{user_id}/get_friends_score\"\n",
+            "\"The number of times the URL was called : 4\"\n",
+            "\"The mean (average) of the response time : 32.5\"\n",
+            "\"The median of the response time : 30.0\"\n",
+            "\"The mode of the response time : 30\"\n",
+            "\"The \\\"dyno\\\" that responded the most : web.2\"\n"
+          ].join
+        ).to_stdout
+      end
+    end
+  end
+end
